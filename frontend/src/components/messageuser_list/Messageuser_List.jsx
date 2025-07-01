745bfd1adca5494afb2e22967640c4df05b42154
kasimnetarioinnovations <kasimnetarioinnovations@gmail.com>
2025-07-01 17:07:14 +0530

    pdate

---
+++ frontend/src/components/messageuser_list/Messageuser_List.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Messageuser_List.css'
+
+const Messageuser_List = () => {
+  return (
+    <div>
+       <div className="message-container" style={{backgroundColor:"white"}}>
+           <div className="message-header d-flex align-items-center justify-content-between">
+              <div>
+                <div>
+                    <img src={message-user-logo} alt="message-user-logo" />
+                </div>
+              </div>
+              <div></div>
+           </div>
+       </div>
+    </div>
+  );
+}
+
+export default Messageuser_List;
