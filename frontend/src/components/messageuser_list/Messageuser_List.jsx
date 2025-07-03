968572025e7059b443598bffc2904f6e28b5124d
kasimnetarioinnovations <kasimnetarioinnovations@gmail.com>
2025-07-03 15:24:53 +0530

    updated offline online

--- frontend/src/components/messageuser_list/Messageuser_List.jsx
+++ frontend/src/components/messageuser_list/Messageuser_List.jsx
@@ -88,9 +88,9 @@ const Messageuser_List = ({ selectedUser }) => {
 
   socket.emit("user-connected", currentuser._id); // Send current user ID
 
-  socket.on("getOnlineUsers", (users) => {
-    setOnlineUsers(users);
-  });
+ socket.on("update-user-status", (users) => {
+  setOnlineUsers(users);
+});
 
   return () => {
     socket.disconnect(); // Clean up
